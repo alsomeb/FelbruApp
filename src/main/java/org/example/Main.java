2098127c8fff84f52356cc47410e88c3977d3120
dafe2201 <daniel_fellden@hotmail.com>
2022-10-27 21:26:53 +0200

    Added Link to Excalidraw diagram.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 import org.example.frontend.FelbruApp;
 
 public class Main {
+
+    //TODO: UPPDATERA LÖPANDE
+    /*https://excalidraw.com/#json=W4Ui3fR0ck4YJoCdGcjuE,5-ppY9WiHrHqH8-KwcY5og*/
     public static void main(String[] args) {
         new FelbruApp();
     }
