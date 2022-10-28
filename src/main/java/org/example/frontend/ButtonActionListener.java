c1dd5cd2ee711763803b490442eef0d417eb27a7
alsomeb <alsomeb@gmail.com>
2022-10-28 17:23:21 +0200

    Collab Session Daniel, Alex. Figuring out game logic. Going from 2d Array to Point system

---
+++ src/main/java/org/example/frontend/ButtonActionListener.java
@@ -0,0 +1,75 @@
+package org.example.frontend;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class ButtonActionListener implements ActionListener {
+
+    private JButton button;
+
+    private JButton[][] buttonGrid;
+
+    public ButtonActionListener(JButton button, JButton[][] buttonGrid) {
+        this.button = button;
+        this.buttonGrid = buttonGrid;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton selectedBtn = (JButton) e.getSource(); // Den knappen man klickat på
+
+        for (int row = 0; row < buttonGrid.length; row++) {
+            for (int col = 0; col < buttonGrid.length; col++) {
+                if (buttonGrid[row][col] == selectedBtn) {
+                    System.out.println(selectedBtn.getText() + " KNAPPENS NR");
+                    System.out.println(row + ", " + col + " KNAPPENS POS");
+                    List<Integer> blank = getCurrentEmptyButtonPosition(buttonGrid);
+                    System.out.println(blank + ", BLANK POS");
+                    JButton blankBtn = buttonGrid[blank.get(0)][blank.get(1)];
+                    isMoveLegal(blankBtn, buttonGrid, row, col);
+                        // Tillåt move
+                }
+            }
+        }
+    }
+
+    public List<Integer> getCurrentEmptyButtonPosition(JButton[][] buttonGrid) {
+
+//        int SelectedPanelPosition;
+        for (int row = 0; row < buttonGrid.length; row++) {
+            for (int col = 0; col < buttonGrid.length; col++) {
+                if(buttonGrid[row][col].getText().isBlank()){
+                    return new ArrayList<>(List.of(row, col));
+                }
+            }
+        }
+        throw new NoSuchElementException(); // Finns ej
+    }
+
+    public boolean isMoveLegal(JButton blankBtn, JButton[][] buttonGrid, int currentRow, int currentCol){
+        if (buttonGrid[currentRow][currentCol-1] == blankBtn || buttonGrid[currentRow][currentCol+1] == blankBtn ||
+                buttonGrid[currentRow+1][currentCol] == blankBtn ||
+                buttonGrid[currentRow-1][currentCol] == blankBtn){
+            System.out.println("True");
+            return true;
+        }
+        else{
+            System.out.println("False");
+            return false;
+        }
+
+
+            /*bool adjacentIsBlank(int selectedGrid, int currentBlankGrid){
+
+if (selectedGrid[row][collumn-1] == currentBlankGrid || selectedGrid [row-1][collumn] etc...){
+return true;
+}
+
+*/
+    }
+}
+
--- src/main/java/org/example/frontend/GamePanel.java
+++ src/main/java/org/example/frontend/GamePanel.java
@@ -1,18 +1,16 @@
 package org.example.frontend;
 
-import org.example.backend.Piece;
 import org.example.backend.ShufflePieces;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Arrays;
 import java.util.List;
 
 public class GamePanel extends JPanel {
     private final int ROWS = 4;
     private final int COLS = 4;
     private final int GAP = 2;
-    private JButton[][] buttonGrid = new JButton[ROWS][COLS];
+    private JButton[][] buttonGrid;
 
 
 
@@ -24,36 +22,61 @@ public class GamePanel extends JPanel {
         this.setLayout(new GridLayout(ROWS, COLS, GAP, GAP));
         this.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         this.setBackground(Color.BLACK);
-        loadPieces();
+        buttonGrid = new JButton[ROWS][COLS];
+        buttonGrid = createInitialButtonGrid();
+
+        Component[] components = this.getComponents();
+        for (int i = 0; i < components.length; i++) {
+            String text = ((JButton)components[i]).getText();
+            if(text.isBlank()) {
+                System.out.println(i);
+            }
+        }
 
         // Game Panel
         //this.setLayout(new GridLayout(4, 4));
 
+        //TODO: ev. obsolet.
+//        ActionListener buttonListener = e -> {
+//            JButton selectedBtn = (JButton) e.getSource();
+//            for (int row = 0; row < buttonGrid.length; row++) {
+//                for (int col = 0; col < buttonGrid[row].length; col++) {
+//                    if (buttonGrid[row][col] == selectedBtn) {
+//                        System.out.printf("Selected row and column: %d %d%n", row, col);
+//                    }
+//                }
+//            }
+//        };
+
     }
 
 
-    private void loadPieces(){
 
-        //shuffledList.forEach(index -> this.add(new PiecePanel(new Piece(index))));
-        //System.out.println(shuffledList);
 
+    private JButton[][] createInitialButtonGrid(){
         int counter = 0;
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid.length; j++){
-                grid[i][j] = shuffledList.get(counter); // Matris av spelplan kommer se ut
-//                this.add(buttonGrid[i][j]);
+                //grid[i][j] = shuffledList.get(counter); // Matris av spelplan kommer se ut
+                buttonGrid[i][j] = new JButton(String.valueOf(shuffledList.get(counter)));
+                buttonGrid[i][j].addActionListener(new ButtonActionListener(buttonGrid[i][j], buttonGrid));
+                this.add(buttonGrid[i][j]);
+                if(buttonGrid[i][j].getText().equals("0")) {
+                    buttonGrid[i][j].setText("");
+                    buttonGrid[i][j].setEnabled(false);
+                }
                 counter++;
             }
         }
-        System.out.println(Arrays.deepToString(grid));
+        return buttonGrid;
     }
 
 
-    public int[][] getGrid() {
-        return grid;
+    public JButton[][] getButtonGrid() {
+        return buttonGrid;
     }
 
-    public void setGrid(int[][] grid) {
-        this.grid = grid;
+    public void setButtonGrid(JButton[][] buttonGrid) {
+        this.buttonGrid = buttonGrid;
     }
 }
