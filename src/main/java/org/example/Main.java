07b851925457053abf315f5b5685d0c19c643332
dafe2201 <daniel_fellden@hotmail.com>
2022-11-02 19:31:01 +0100

    * Added updated LINK to Excalidraw for live demo.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 import org.example.frontend.FelbruApp;
 
 public class Main {
-//    https://excalidraw.com/#json=CDWQGEbRN4ZVtrEsg8kBW,pXa6q_BfdJdLOuTuDRAwhA
+//    https://excalidraw.com/#json=CDWQGEbRN4 ZVtrEsg8kBW,pXa6q_BfdJdLOuTuDRAwhA
     public static void main(String[] args) {
         new FelbruApp();
     }
